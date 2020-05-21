3f659212220824be1a13738f4a9a2db6593c8f00
otavianocotemig <otaviano@cotemig.com.br>
2020-05-21 16:18:12 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAtleta3D2/Atleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAtleta3D2
+{
+    class Atleta
+    {
+        private String nome;
+        private double peso;
+        private double altura;
+
+    }
+}
